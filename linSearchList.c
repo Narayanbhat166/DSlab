55fddcbebeff33bfbfffdcef158036570458a0fa
Narayan Bhat <bhatj1964@gmail.com>
2019-09-07 04:01:58 -0700

    Implemented linSearchByPos function

--- linSearchList.c
+++ linSearchList.c
@@ -2,6 +2,7 @@
 
 
 void linSearchByKey(node F);
+void linSearchByPos(node F);
 
 void main() {
 
@@ -10,8 +11,8 @@ void main() {
 
 	while(1) {
 
-		printf("Enter your choice:\n");
-		printf("1:InsertFront\n2:InsertRear\n3:DeleteFront\n4:DeleteRear\n5:display\n6:Search Item By Key\n");
+		printf("\nEnter your choice:\n");
+		printf("1:InsertFront\n2:InsertRear\n3:DeleteFront\n4:DeleteRear\n5:display\n6:Search Item By Key\n7:Search Item by position\n");
 
 		scanf("%d",&choice);
 	
@@ -28,6 +29,8 @@ void main() {
 				   break;
 			case 6:linSearchByKey(First);
 				   break;
+			case 7:linSearchByPos(First);
+				   break;
 			default:exit(0);
 		}
 	}
@@ -41,7 +44,7 @@ void linSearchByKey(node F) {
 
 	int item, pos = 1;
 
-	printf("Enter the item");
+	printf("\nEnter the item\n");
 	scanf("%d",&item);
 
 	while(F != NULL) {
@@ -59,5 +62,30 @@ void linSearchByKey(node F) {
 		printf("%d does not exist in the list\n", item);
 		return;
 	}
+}
+
+void linSearchByPos(node F) {
+	if(F == NULL) {
+		printf("\nThe list is empty\n");
+		return;
+	}
+
+	int item, pos ,count = 1;
+
+	printf("\nEnter the position\n");
+	scanf("%d",&pos);
+
+	while(F != NULL && count != pos) {
+		F = F->link;
+		count++;
+	}
+
+	if(F == NULL) {
+		printf("%d is an invalid position\n", pos);
+		return;
+	}
+
+	printf("\nThe item present at pos %d is %d\n",pos, F->info);
+
 }
 
